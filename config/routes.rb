c92e34c7f4fd17a22587f9e614a9012c77191663
harisree06 <harisree@gmail.com>
2018-04-07 18:12:09 -0400

    ruby testing app authentication 9

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'version1' do
       resources :steps, only: [:index, :show],  param: :name
-      get 'code/ruby'
-      post 'code/ruby' => 'code#post_ruby',  param: :code
+     
+      post 'code/ruby' => 'codes#post_ruby',  param: :code
     end
   end
 end
