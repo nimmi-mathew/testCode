6ddb846c0ea231e10f68d200dde2de68b8eed395
harisree06 <harisree@gmail.com>
2018-04-07 18:19:50 -0400

    ruby testing app authentication 11

--- app/controllers/api/version1/code_controller.rb
+++ app/controllers/api/version1/code_controller.rb
@@ -9,6 +9,7 @@ module Api
              require 'sicuro'
             @code = params[:code]
             @codeResult = Sicuro.eval(@code)
+            p "######################################## #{@codeResult}"
             render json:  CGI::escapeHTML(@codeResult.stdout.to_s).gsub(/(?:\n\r?|\r\n?)/, '<br>')
          end
       end
