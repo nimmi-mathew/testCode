c010f41a978d237b045c2b1c91f0e2ae3e41bec5
harisree06 <harisree@gmail.com>
2018-04-13 00:16:21 -0400

    fixed issue of lessons with spaces

--- app/controllers/api/version1/steps_controller.rb
+++ app/controllers/api/version1/steps_controller.rb
@@ -3,7 +3,8 @@ module Api
       class StepsController < ApplicationController
         skip_before_action :verify_authenticity_token , raise: false
         def show
-            chapter = Chapter.find_by_name(params[:name])
+            name = params[:name].gsub('_',' ')
+            chapter = Chapter.find_by_name(name)
             p ".................................#{chapter}"
             @steps = chapter.steps
             code_test = []
