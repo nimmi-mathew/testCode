7ce86f886235a29388daa10e15127ba64653d3b0
harisree06 <harisree@gmail.com>
2018-04-13 10:23:24 -0400

    corrected the message and added logger info

--- app/controllers/api/version1/codes_controller.rb
+++ app/controllers/api/version1/codes_controller.rb
@@ -3,7 +3,9 @@ module Api
       class CodesController < ApplicationController
         skip_before_action :verify_authenticity_token , raise: false
 
+        Rails.logger = Logger.new(STDOUT)
         
+        #function thats invoked when the post request is placed
         def post_ruby
             
             code = params[:code]
@@ -14,33 +16,33 @@ module Api
             @step.code_tests.each do |test|
             code = validateCode(code)
             @testcode = code + "\n print " + test.input 
-                t = testResult(@testcode)
-                p"....................................#{t}"
-              if t.to_s == test.output 
+            testResult = testResult(@testcode)
+            logger.info"..............................#{testResult}"
+                if testResult.to_s == test.output 
                  @valid += 1 
                end
        
                
            end
-           @message = "#{@valid} out of #{@testCout} testcases passed"
+           @message = "#{@valid} out of #{@testCount} testcases passed"
            render json: {status: 'SUCCESS', testPass: @valid, testCount: @testCount, message:@message },status: :ok
-    end
-
-    def validateCode(code)
+        end
+        #function to format the code
+        def validateCode(code)
      
-      code = code.split("\n")
-      code.delete_if {|w| w.include? "puts"}
-      code.delete_if {|w| w.include? "print" }
-      code.join("\n")
+        code = code.split("\n")
+        code.delete_if {|w| w.include? "puts"}
+        code.delete_if {|w| w.include? "print" }
+        code.join("\n")
+        end
+        #function to evaluate the formated code
+        def testResult(code)
+          require 'sicuro'
+            logger.info"................................#{code}"
+          @codeResult = Sicuro.eval(code)
+          logger.info "............................ #{@codeResult}"
+          (@codeResult.stdout).gsub(/(?:\n\r?|\r\n?)/, '<br>')
+        end
+      end
     end
-
-    def testResult(code)
-      require 'sicuro'
-      p code
-      @codeResult = Sicuro.eval(code)
-      p "............................ #{@codeResult}"
-      (@codeResult.stdout).gsub(/(?:\n\r?|\r\n?)/, '<br>')
-    end
-end
-end
 end
